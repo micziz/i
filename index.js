584c79dc70544f0ccd455ed1da40a187f6464c78
micziz <miczicontent@gmail.com>
2023-02-22 16:52:56 +0200

    Add / page

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (req, res) => {
+    res.send('<h1>Still in development</h1>')
+})
+
 app.get('/:pgk', (req, res) => {
     res.redirect(`https://raw.githubusercontent.com/micziz/i/main/installers/${req.params.pgk}.sh`)
 })
